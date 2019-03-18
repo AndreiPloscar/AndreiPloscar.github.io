d21678d94af2afd383b74368ddeb9b1861bd6ea7
”AndreiPloscar” <andrei.ploscar@yahoo.com>
2019-03-18 19:55:28 +0200

    show skills with for

--- intro.js
+++ intro.js
@@ -58,3 +58,12 @@ pages[i].style.display = 'none';
 
 initMenu(); 
 
+function showSkills() {
+    var skills = ['html', 'css', 'js'];
+    console.warn('showSkills', skills);
+    for(var i = 0; i < skills.length; i++) {
+        console.info(i + 1 + ". " + skills[i]);
+    }
+}
+
+showSkills();
